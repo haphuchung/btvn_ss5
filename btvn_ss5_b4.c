bca1c6f2918a8071aca2ee2930cb26f70968c776
haphuchung <haphuchung2006@gmail.com>
2024-11-25 20:22:47 +0700

    Add files via upload

---
+++ btvn_ss5_b4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i; 
+
+    do {
+        printf("Nhap mot so nguyen tu 1 den 10: ");
+        scanf("%d", &n);
+        if (n < 1 || n > 10) {
+            printf("So nhap khong hop le. Vui long thu lai.\n");
+        }
+    } while (n < 1 || n > 10); 
+
+    printf("Bang cuu chuong cua %d:\n", n);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
